d60cd9be712d3de44ed4145440079cc337b866f5
denblo <Denis.Blokhin@transas.com>
2018-02-06 17:52:53 +0300

    update port

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 
     log.Print("Listening on port 8080")
-    log.Fatal(http.ListenAndServe(":8087", nil))
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
