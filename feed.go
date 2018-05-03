6430a32fc1cf4cb4c75a9cd95b015cc5c80e3870
denblo <Denis.Blokhin@transas.com>
2018-05-03 19:07:33 +0300

    wip 7.0

--- feed.go
+++ feed.go
@@ -59,10 +59,10 @@ func getGoQuery(url string, ctx *context.Context) (*goquery.Document, error) {
 	return doc, err
 }
 
-func getFileLink(playerUrl string, ttl int, ctx *context.Context) string {
+func getFileLink(playerURL string, ttl int, ctx *context.Context) string {
 
-	playerdoc, err := getGoQuery(playerUrl, ctx)
-	var fileLink string = ""
+	playerdoc, err := getGoQuery(playerURL, ctx)
+	var fileLink string
 
 	if err != nil {
 		fmt.Print("Error\n")
@@ -72,34 +72,33 @@ func getFileLink(playerUrl string, ttl int, ctx *context.Context) string {
 	}
 
 	if fileLink == "" && ttl > 0 {
-		return getFileLink(playerUrl, ttl-1, ctx)
+		return getFileLink(playerURL, ttl-1, ctx)
 	}
 	return fileLink
 
 }
 
-func GetFileUrl(link string, ctx *context.Context) string {
+func GetFileURL(link string, ctx *context.Context) string {
 
 	subdoc, err := getGoQuery(link, ctx)
 	if err != nil {
 		return ""
-	} else {
-
-		title := subdoc.Find(".current").Text()
-
-		iframe := subdoc.Find("[name='embeddedPlayer']")
-		playerSrc, _ := iframe.Attr("src")
-
-		date := subdoc.Find(".post_date .date").Text()
-		month := subdoc.Find(".post_date .month").Text()
-		year := subdoc.Find(".post_date .year").Text()
-
-		hasher := sha1.New()
-		hasher.Write([]byte(year + "-" + month + "-" + date + ": " + title))
-		playerUrl, _ := url.Parse("http:" + playerSrc)
-
-		return getFileLink(playerUrl.String(), 5, ctx)
 	}
+
+	title := subdoc.Find(".current").Text()
+
+	iframe := subdoc.Find("[name='embeddedPlayer']")
+	playerSrc, _ := iframe.Attr("src")
+
+	date := subdoc.Find(".post_date .date").Text()
+	month := subdoc.Find(".post_date .month").Text()
+	year := subdoc.Find(".post_date .year").Text()
+
+	hasher := sha1.New()
+	hasher.Write([]byte(year + "-" + month + "-" + date + ": " + title))
+	playerURL, _ := url.Parse("http:" + playerSrc)
+
+	return getFileLink(playerURL.String(), 5, ctx)
 }
 
 func ScrapePageItems(page int, url string, ctx *context.Context) []*feeds.Item {
@@ -140,6 +139,76 @@ func ScrapePageItems(page int, url string, ctx *context.Context) []*feeds.Item {
 		}
 
 		items = append(items, feedItem)
+
+	})
+	return items
+}
+
+func GetFeedFromRecords(records *[]Record) string {
+	var items []*feeds.Item
+	for _, record := range *records {
+		feedItem := &feeds.Item{
+			Id:          record.ID,
+			Title:       record.Title,
+			Link:        &feeds.Link{Href: record.Link},
+			Enclosure:   &feeds.Enclosure{Url: record.FileLink, Length: "0", Type: "audio/mpeg"},
+			Created:     time.Unix(int64(record.Created), 0),
+			Description: record.Description,
+		}
+
+		items = append(items, feedItem)
+	}
+
+	feed := &feeds.Feed{
+		Title:       "Custom Feed",
+		Link:        &feeds.Link{Href: ""},
+		Description: "Custom feed audios",
+		Items:       items,
+	}
+
+	rss, _ := feed.ToRss()
+
+	return rss
+}
+
+func ScrapeRecords(page int, url string, ctx *context.Context) []*Record {
+	fmt.Printf("scrape page %d at %s \n", page, url)
+	doc, err := getGoQuery(fmt.Sprintf(url+"/page/%d", page), ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var items []*Record
+
+	doc.Find("article").Each(func(index int, item *goquery.Selection) {
+		linkTag := item.Find(".post_info a")
+		link, _ := linkTag.Attr("href")
+
+		title := strings.TrimSpace(item.Find(".post_text h2").Text())
+		description := strings.TrimSpace(item.Find(".post_text h3").Text())
+
+		date := item.Find(".post_date .date").Text()
+		month := item.Find(".post_date .month").Text()
+		year := item.Find(".post_date .year").Text()
+
+		hasher := sha1.New()
+		hasher.Write([]byte(year + "-" + month + "-" + date + ": " + title))
+		id := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+
+		published, _ := time.Parse(shortForm, year+"-"+month+"-"+date)
+
+		fileLink := strings.Replace(link, "http://svoeradio.fm/", "http://svoe-feed.appspot.com/file/", 1)
+
+		record := &Record{
+			ID:          id,
+			Title:       title,
+			Link:        link,
+			FileLink:    fileLink,
+			Description: description,
+			Created:     int(published.Unix()),
+		}
+
+		items = append(items, record)
 
 	})
 	return items
--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"regexp"
 
+	"github.com/gorilla/mux"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
 )
+
+type Record struct {
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+	FileLink    string `json:"fileLink"`
+	Created     int    `json:"created"`
+	Readed      bool   `json:"readed"`
+	Current     bool   `json:"current"`
+}
 
 func main() {
 	registerHandlers()
@@ -14,10 +30,20 @@ func main() {
 }
 
 func registerHandlers() {
-	http.HandleFunc("/", handle)
-	http.HandleFunc("/_ah/health", healthCheckHandler)
-	http.HandleFunc("/feed", feedHandler)
-	http.HandleFunc("/file/", fileProxyHandler)
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	r.HandleFunc("/_ah/health", healthCheckHandler)
+	r.HandleFunc("/feed", feedHandler)
+	r.HandleFunc("/file/", fileProxyHandler)
+	r.HandleFunc("/svoe-migrate", migrateSvoeRecords).Methods("GET")
+	r.HandleFunc("/records", getRecordsHandler).Methods("GET")
+	r.HandleFunc("/records/current", getCurrentRecordsHandler).Methods("GET")
+	r.HandleFunc("/records/current/{id}", setCurrentRecordHandler).Methods("PUT")
+	//r.HandleFunc("/records/unreaded", getRecordsHandler).Methods("GET")
+
+	http.Handle("/", r)
+	//http.HandleFunc("/", handle)
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +51,16 @@ func handle(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello world!")
+
+	ctx := appengine.NewContext(r)
+	record := &Record{Title: "test title", Description: "test description", Link: "test url"}
+	key := datastore.NewIncompleteKey(ctx, "Records", nil)
+
+	key, err := datastore.Put(ctx, key, record)
+
+	_ = err
+
+	fmt.Fprint(w, "Hello world!", key)
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
@@ -34,10 +69,22 @@ func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
 func feedHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-	rss := ScrapeFeed(&ctx)
 
-	w.Header().Set("Content-Type", "application/xml")
-	fmt.Fprint(w, rss)
+	q := datastore.NewQuery("Records").
+		Order("-Created")
+
+	var records []Record
+
+	_, err := q.GetAll(ctx, &records)
+
+	if err == nil {
+		rss := GetFeedFromRecords(&records)
+		w.Header().Set("Content-Type", "application/xml")
+		fmt.Fprint(w, rss)
+	} else {
+		fmt.Fprint(w, err)
+	}
+
 }
 
 func fileProxyHandler(w http.ResponseWriter, r *http.Request) {
@@ -45,12 +92,107 @@ func fileProxyHandler(w http.ResponseWriter, r *http.Request) {
 	re := regexp.MustCompile("/file/(.+)")
 	match := re.FindStringSubmatch(r.URL.EscapedPath())
 
-	fileUrl := GetFileUrl("http://svoeradio.fm/"+match[1], &ctx)
+	fileURL := GetFileURL("http://svoeradio.fm/"+match[1], &ctx)
 
-	if fileUrl != "" {
-		http.Redirect(w, r, fileUrl, 301)
+	if fileURL != "" {
+		http.Redirect(w, r, fileURL, 301)
 	} else {
 		fmt.Fprint(w, "Bad file url: ", match[1])
 	}
+
+}
+
+func getRecordsHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	q := datastore.NewQuery("Records")
+
+	var records []Record
+
+	q.GetAll(ctx, &records)
+
+	json.NewEncoder(w).Encode(records)
+}
+
+func getCurrentRecordsHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	q := datastore.NewQuery("Records").Filter("Current =", true)
+
+	var records []Record
+
+	q.GetAll(ctx, &records)
+
+	if records == nil {
+		records = []Record{}
+	}
+
+	json.NewEncoder(w).Encode(records)
+}
+
+func getRecordByID(id string, ctx *context.Context) (*datastore.Key, *Record) {
+	log.Debugf(*ctx, "Get record by id %s", id)
+	q := datastore.NewQuery("Records").
+		Filter("ID =", id).
+		Limit(50).
+		Offset(50)
+
+	var records []Record
+	keys, err := q.GetAll(*ctx, &records)
+
+	if err != nil {
+		log.Errorf(*ctx, "Error on query %v", err)
+	}
+
+	if records != nil {
+		log.Debugf(*ctx, "Finded")
+		return keys[0], &records[0]
+	}
+	log.Debugf(*ctx, "NotFinded")
+	return nil, nil
+}
+
+func setCurrentRecordHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	ctx := appengine.NewContext(r)
+
+	key, record := getRecordByID(vars["id"], &ctx)
+
+	if record != nil {
+		record.Current = true
+		datastore.Put(ctx, key, record)
+
+		json.NewEncoder(w).Encode(record)
+	}
+
+}
+
+func addOrUpdateRecord(record *Record, ctx *context.Context) {
+	key, _ := getRecordByID(record.ID, ctx)
+
+	if key == nil {
+		key = datastore.NewIncompleteKey(*ctx, "Records", nil)
+	}
+
+	datastore.Put(*ctx, key, record)
+}
+
+func migrateSvoeRecords(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
+	for i := 0; true; i++ {
+		records := ScrapeRecords(i, "http://svoeradio.fm/archive/audio-archive", &ctx)
+
+		if len(records) == 0 {
+			break
+		}
+
+		for _, record := range records {
+			addOrUpdateRecord(record, &ctx)
+		}
+
+	}
+
+	fmt.Fprint(w, "Done")
 
 }
