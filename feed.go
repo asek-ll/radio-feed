994361950187d1459ceb5f0ec0c1a2cda745b0c7
denblo <Denis.Blokhin@transas.com>
2018-07-17 14:38:27 +0300

    some cleanum, add retrying for page scrapping

--- feed.go
+++ feed.go
@@ -6,7 +6,6 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"fmt"
-	"log"
 	"net/url"
 	"strings"
 	"time"
@@ -17,39 +16,6 @@ import (
 )
 
 const shortForm = "2006-Jan-02"
-
-func ScrapeFeed(ctx *context.Context) string {
-	feed := &feeds.Feed{
-		Title:       "Своё Радио",
-		Link:        &feeds.Link{Href: "https://youtube.com/channel/UCpT2HfwVbQTkDNA-chwgRew"},
-		Description: "Официальный канал радиостанции Своё Радио. Архивы",
-		Items:       ScrapePageItems(1, "http://svoeradio.fm/archive/audio-archive", ctx),
-	}
-
-	rss, _ := feed.ToRss()
-
-	return rss
-}
-
-func ScrapeAllAudioArchives(ctx *context.Context) string {
-	feed := &feeds.Feed{
-		Title:       "Своё Радио",
-		Link:        &feeds.Link{Href: "https://youtube.com/channel/UCpT2HfwVbQTkDNA-chwgRew"},
-		Description: "Официальный канал радиостанции Своё Радио. Архивы",
-	}
-
-	for i := 1; true; i++ {
-		items := ScrapePageItems(i, "http://svoeradio.fm/archive/audio-archive", ctx)
-		if len(items) == 0 {
-			break
-		}
-		feed.Items = append(feed.Items, items...)
-	}
-
-	rss, _ := feed.ToRss()
-
-	return rss
-}
 
 func getGoQuery(url string, ctx *context.Context) (*goquery.Document, error) {
 	client := urlfetch.Client(*ctx)
@@ -105,49 +71,6 @@ func GetFileURL(link string, ctx *context.Context) string {
 	return getFileLink(playerURL.String(), 5, ctx)
 }
 
-func ScrapePageItems(page int, url string, ctx *context.Context) []*feeds.Item {
-	fmt.Printf("scrape page %d at %s \n", page, url)
-	doc, err := getGoQuery(fmt.Sprintf(url+"/page/%d", page), ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var items []*feeds.Item
-
-	doc.Find("article").Each(func(index int, item *goquery.Selection) {
-		linkTag := item.Find(".post_info a")
-		link, _ := linkTag.Attr("href")
-
-		title := strings.TrimSpace(item.Find(".post_text h2").Text())
-		description := strings.TrimSpace(item.Find(".post_text h3").Text())
-
-		date := item.Find(".post_date .date").Text()
-		month := item.Find(".post_date .month").Text()
-		year := item.Find(".post_date .year").Text()
-
-		hasher := sha1.New()
-		hasher.Write([]byte(year + "-" + month + "-" + date + ": " + title))
-		id := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-
-		published, _ := time.Parse(shortForm, year+"-"+month+"-"+date)
-
-		fileLink := strings.Replace(link, "http://svoeradio.fm/", "http://svoe-feed.appspot.com/file/", 1)
-
-		feedItem := &feeds.Item{
-			Id:          id,
-			Title:       title,
-			Link:        &feeds.Link{Href: link},
-			Enclosure:   &feeds.Enclosure{Url: fileLink, Length: "0", Type: "audio/mpeg"},
-			Created:     published,
-			Description: description,
-		}
-
-		items = append(items, feedItem)
-
-	})
-	return items
-}
-
 func GetFeedFromRecords(records *[]Record) string {
 	var items []*feeds.Item
 	for _, record := range *records {
@@ -174,12 +97,23 @@ func GetFeedFromRecords(records *[]Record) string {
 
 	return rss
 }
-
 func ScrapeRecords(page int, url string, ctx *context.Context) *[]Record {
+
+	for i := 0; i < 5; i++ {
+		records, err := scrapeRecords(page, url, ctx)
+		if err == nil {
+			return records
+		}
+	}
+
+	return &[]Record{}
+}
+
+func scrapeRecords(page int, url string, ctx *context.Context) (*[]Record, error) {
 	fmt.Printf("scrape page %d at %s \n", page, url)
 	doc, err := getGoQuery(fmt.Sprintf(url+"/page/%d", page), ctx)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	var items []Record
@@ -215,5 +149,5 @@ func ScrapeRecords(page int, url string, ctx *context.Context) *[]Record {
 		items = append(items, record)
 
 	})
-	return &items
+	return &items, nil
 }
